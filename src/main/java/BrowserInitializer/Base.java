8afa6f399919d70dd51446efa0aa7dc379cc78b7
ankur250493 <ank11.thakur@gmail.com>
2021-03-10 01:46:05 +0530

    Update Base.java

--- src/main/java/BrowserInitializer/Base.java
+++ src/main/java/BrowserInitializer/Base.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
@@ -16,13 +17,15 @@ import java.util.concurrent.TimeUnit;
 public class Base {
     public WebDriver driver;
     public Properties prop = new Properties();
+
     public WebDriver InitialiseDemo() throws IOException {
 
 
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/resources/Data.properties");
         prop.load(fis);
         String browserName = System.getProperty("browser");
-      //  String browserName = prop.getProperty("browser");
+        //  String browserName = prop.getProperty("browser");
+
 
         switch (browserName) {
             case "firefox":
@@ -31,8 +34,15 @@ public class Base {
                 break;
             case "chrome":
                 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/DriverFiles/chromedriver");
-                driver = new ChromeDriver();
-                driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("disable-infobars"); // disabling infobars
+                options.addArguments("--disable-extensions"); // disabling extensions
+                options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+                options.addArguments("--no-sandbox"); // Bypass OS security model
+
+                driver = new ChromeDriver(options);
+                driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
                 driver.manage().window().maximize();
                 break;
 
@@ -45,9 +55,9 @@ public class Base {
 
     public String getScreenshotPath(String testCaseName, WebDriver driver) throws IOException {
         TakesScreenshot ts = (TakesScreenshot) driver;
-        File source =  ts.getScreenshotAs(OutputType.FILE);
+        File source = ts.getScreenshotAs(OutputType.FILE);
         String destinationFile = System.getProperty("user.dir") + "/screenshot/reports/" + testCaseName + ".png";
-        FileUtils.copyFile(source,new File(destinationFile));
+        FileUtils.copyFile(source, new File(destinationFile));
         return destinationFile;
 
     }
